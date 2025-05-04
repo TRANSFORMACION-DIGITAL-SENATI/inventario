96d62600aca64c7d3101fb0e9891cc64f79a1d5a
djarana <djaranamarinera@gmail.com>
2025-05-04 14:19:14 +0000

    feat: Actualizar modelo Alumnos con nuevos campos/métodos

--- app/Models/Alumnos.php
+++ app/Models/Alumnos.php
@@ -7,4 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class Alumnos extends Model
 {
     //
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     */
+    protected $table = 'alumnos';
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'email',
+        'fecha_nacimiento',
+        'direccion',
+        'telefono'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     */
+    protected $casts = [
+        'fecha_nacimiento' => 'date',
+    ];
+
+    /**
+     * Get the full name of the student.
+     */
+    public function getFullNameAttribute()
+    {
+        return "{$this->nombre} {$this->apellido}";
+    }
 }
